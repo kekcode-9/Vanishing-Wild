d50bc5817880b5e1f21b3e3fbb59f3c79c970c9a
kekcode-9 <rupamita.s97@gmail.com>
2025-07-26 11:35:39 +0530

    removed redundant component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 "use client"
 import SightingsMapTimeline from "@/components/livingPlanetIndex/SightingsMapTimeline";
-import PopulationTrendAreaChart from "@/components/livingPlanetIndex/populationTrend/PopulationTrendAreaChart";
 
 export default function Home() {
 
   return (
     <div className="MainDiv" style={{width: "100vw", height: "100vh"}}>
-      <PopulationTrendAreaChart />
     </div>
   );
 }
